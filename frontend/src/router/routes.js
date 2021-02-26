4c1589fcd5fd474b0ad411ef5da10d4fba374217
johnckealy <johnckealy@gmail.com>
2021-02-26 15:25:04 +0000

    removed all Django stuff and app now working

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -4,24 +4,6 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-<<<<<<< HEAD
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: '/login', component: () => import('pages/login.vue') },
-    ]
-  },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      {
-        path: '/dashboard',
-        component: () => import('pages/dashboard.vue'),
-        meta: { requiresAuth: true },
-      },
-    ]
-  },
-  { // 404 page
-=======
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
@@ -29,7 +11,6 @@ const routes = [
   // Always leave this as last one,
   // but you can also remove it
   {
->>>>>>> base
     path: '*',
     component: () => import('pages/Error404.vue')
   }
